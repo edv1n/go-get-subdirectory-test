037af173b72d1e97fc32d76938d90f56530bf755
Edwin S <54180289+edv1n@users.noreply.github.com>
2025-10-02 12:06:14 +0000

    added go code for displaying content on godoc

---
+++ gopkg/consts.go
@@ -0,0 +1,7 @@
+package gopkg
+
+const (
+	Module = "edv1n.github.io/go-get-subdirectory-test"
+	Repo   = "https://github.com/edv1n/go-get-subdirectory-test"
+	Subdir = "gopkg"
+)
---
+++ gopkg/funcs.go
@@ -0,0 +1,5 @@
+package gopkg
+
+func HtmlDocument() string {
+	return "<head>" + MetaTagContent + "</head>"
+}
